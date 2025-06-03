cb7ccf522162eb6fca0858e676cf34843d30f301
kishor2581 <privatework278@gmail.com>
2025-06-03 10:37:48 +0530

    created

---
+++ Swap.java
@@ -0,0 +1,19 @@
+
+
+public class Swap {
+    public static int[] swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+        return new int[]{a, b};
+    }
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+        int[] result = swap(a, b);
+        System.out.println("After swapping: a = " + result[0] + ", b = " + result[1]);
+    }
+}
